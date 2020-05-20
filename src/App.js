a30e8a20567ae9b2b3c3da5c62d591a75212c001
kyle rodgers <kyle77r@gmail.com>
2020-05-20 01:02:40 -0700

    Adding in basic website appearance

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import banner from './assets/airsoftbanner.jpg';
+import logo from './assets/us-airsoft-logo.png';
 import './App.css';
+import {Navbar, Nav, Button} from 'react-bootstrap/';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="staticBG">
+          <div className="App-header">
+            <div className="logobox">
+              <img src={logo} className="logo" />
+            </div>
+            <div className="login">
+              <Button variant="outline-success">Login</Button>
+            </div>
+          </div>
+        <div className="navbar">
+          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar.Brand href="blank">Home</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="blah">Leaderboard</Nav.Link>
+                <Nav.Link href="blah">Pricing</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </div>
+          <div className="banner">
+            <img src={banner} className="bannerimg"/>
+            <div className="overlay"></div>
+            <div className="overlayText">
+              The premier airsoft arena in the United States, Home of the first Stat Tracking & Leaderboard system!
+            </div>
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
