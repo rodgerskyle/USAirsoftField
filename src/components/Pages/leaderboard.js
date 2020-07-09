15b05892e49b1f341843dd3a113c832976af1c04
kyle rodgers <kyle77r@gmail.com>
2020-07-08 22:11:38 -0700

    Added working leaderboard

---
+++ src/components/Pages/leaderboard.js
@@ -0,0 +1,161 @@
+import React, { Component } from 'react';
+import { Table } from 'react-bootstrap/';
+import '../../App.css';
+
+import rankimages from '../constants/smallrankimgs';
+import { Link } from "react-router-dom";
+
+import { withFirebase } from '../Firebase';
+
+class Leaderboards extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            images: rankimages,
+            loading: false,
+            users: [],
+            getRankState: this.getRank,
+        };
+    }
+
+    //Figuring out rank logic
+    getRank(points) {
+        var index = 1;
+        if (points < 125) {
+            index = 1;
+        }
+        else if (points < 225) {
+            index = 2;
+        }
+        else if (points < 335) {
+            index = 3;
+        }
+        else if (points < 450) {
+            index = 4;
+        }
+        else if (points < 570) {
+            index = 5;
+        }
+        else if (points < 690) {
+            index = 6;
+        }
+        else if (points < 820) {
+            index = 7;
+        }
+        else if (points < 960) {
+            index = 8;
+        }
+        else if (points < 1110) {
+            index = 9;
+        }
+        else if (points < 1270) {
+            index = 10;
+        }
+        else if (points < 1440) {
+            index = 11;
+        }
+        else if (points < 1640) {
+            index = 12;
+        }
+        else if (points < 1840) {
+            index = 13;
+        }
+        else if (points < 2090) {
+            index = 14;
+        }
+        else if (points < 2340) {
+            index = 15;
+        }
+        else if (points < 2615) {
+            index = 16;
+        }
+        else if (points < 2890) {
+            index = 17;
+        }
+        else if (points < 3190) {
+            index = 18;
+        }
+        else if (points < 3490) {
+            index = 19;
+        }
+        else if (points < 3790) {
+            index = 20;
+        }
+        else if (points < 4115) {
+            index = 21;
+        }
+        else if (points < 4500) {
+            index = 22;
+        }
+        else {
+            index = 23;
+        }
+        index--;
+        return index;
+    }
+
+    componentDidMount() {
+        this.setState({ loading: true });
+
+        this.props.firebase.users().on('value', snapshot => {
+            const usersObject = snapshot.val();
+
+            const usersList = Object.keys(usersObject).map(key => ({
+                ...usersObject[key],
+                uid: key,
+            }));
+
+            this.setState({
+                users: usersList,
+                loading: false,
+            });
+        });
+    }
+
+    componentWillUnmount() {
+        this.props.firebase.users().off();
+    }
+
+    render() {
+        const { users, loading, getRankState } = this.state;
+
+        return (
+            <div className="pagePlaceholder">
+                <h1>Leaderboards</h1>
+
+                {loading && <div>Loading ...</div>}
+
+                <UserList users={users} images={this.state.images} getRank={getRankState}/>
+            </div>
+        );
+    }
+}
+
+
+const UserList = ({ users, images, getRank}) => (
+    <Table className="table table-striped table-dark">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Rank</th>
+                <th scope="col">Name</th>
+                <th scope="col">Points</th>
+            </tr>
+        </thead>
+        <tbody>
+            {users.sort((a,b) => a.points < b.points ? 1 : -1).map((user, i) => (
+
+                <tr key={user.uid}>
+                    <th scope="row">{i+1}</th>
+                        <td><img src={images.length !== 0 ? images[getRank(user.points)] : null} 
+                        alt="Player Rank" /></td>
+                        <td>{user.name}</td>
+                        <td>{user.points}</td>
+                </tr>
+            ))}
+        </tbody>
+    </Table>
+);
+
+export default withFirebase(Leaderboards);
---
+++ src/components/constants/smallrankimgs.js
@@ -0,0 +1,50 @@
+// smallrankimgs.js
+import rank1 from '../../assets/smallranks/private.png'
+import rank2 from '../../assets/smallranks/privatefirstclass.png'
+import rank3 from '../../assets/smallranks/specialist.png'
+import rank4 from '../../assets/smallranks/corporal.png'
+import rank5 from '../../assets/smallranks/sergeant.png'
+import rank6 from '../../assets/smallranks/staffsergeant.png'
+import rank7 from '../../assets/smallranks/sergeantfirstclass.png'
+import rank8 from '../../assets/smallranks/mastersergeant.png'
+import rank9 from '../../assets/smallranks/firstsergeant.png'
+import rank10 from '../../assets/smallranks/sergeantmajor.png'
+import rank11 from '../../assets/smallranks/commandsergeantmajor.png'
+import rank12 from '../../assets/smallranks/sergeantmajorofthearmy.png'
+import rank13 from '../../assets/smallranks/2ndlt.png'
+import rank14 from '../../assets/smallranks/1stlt.png'
+import rank15 from '../../assets/smallranks/captain.png'
+import rank16 from '../../assets/smallranks/major.png'
+import rank17 from '../../assets/smallranks/lieutenantcol.png'
+import rank18 from '../../assets/smallranks/colonel.png'
+import rank19 from '../../assets/smallranks/1general.png'
+import rank20 from '../../assets/smallranks/2general.png'
+import rank21 from '../../assets/smallranks/3general.png'
+import rank22 from '../../assets/smallranks/4general.png'
+import rank23 from '../../assets/smallranks/5general.png'
+
+export default [
+    rank1,
+    rank2,
+    rank3,
+    rank4,
+    rank5,
+    rank6,
+    rank7,
+    rank8,
+    rank9,
+    rank10,
+    rank11,
+    rank12,
+    rank13,
+    rank14,
+    rank15,
+    rank16,
+    rank17,
+    rank18,
+    rank19,
+    rank20,
+    rank21,
+    rank22,
+    rank23
+]
