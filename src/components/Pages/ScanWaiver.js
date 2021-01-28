c9376dd32c9da4baceebf49c1b20445504784084
kyle rodgers <kyle77r@gmail.com>
2021-01-28 01:19:11 -0800

    Added the ability to scan in waivers via camera

---
+++ src/components/Pages/ScanWaiver.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { Container, Breadcrumb } from 'react-bootstrap';
+
+import '../../App.css';
+
+import { AuthUserContext, withAuthorization } from "../session";
+
+import { compose } from 'recompose';
+import CameraModule from '../constants/CameraModule';
+
+import { LinkContainer } from 'react-router-bootstrap';
+
+import { withFirebase } from '../Firebase';
+
+import * as ROLES from '../constants/roles';
+
+class ScanWaiver extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+            <AuthUserContext.Consumer>
+                {authUser => (
+                    <div className="background-static-all">
+                        <Container>
+                            <h2 className="admin-header">Scan Waiver</h2>
+                            <Breadcrumb className="admin-breadcrumb">
+                                {authUser && !!authUser.roles[ROLES.ADMIN] ?
+                                    <LinkContainer to="/admin">
+                                        <Breadcrumb.Item>Admin</Breadcrumb.Item>
+                                    </LinkContainer>
+                                    :
+                                    <LinkContainer to="/dashboard">
+                                        <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
+                                    </LinkContainer>
+                                }
+                                <Breadcrumb.Item active>Scan Waiver</Breadcrumb.Item>
+                            </Breadcrumb>
+                            <CameraModule />
+                        </Container>
+                    </div>
+                )}
+            </AuthUserContext.Consumer>
+        )
+    }
+}
+
+const condition = authUser =>
+    authUser && (!!authUser.roles[ROLES.ADMIN] || !!authUser.roles[ROLES.WAIVER]);
+
+export default compose(
+    withAuthorization(condition),
+    withFirebase,
+)(ScanWaiver);
---
+++ src/components/constants/ImagePreview.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import '../../App.css';
+import './ImagePreview.css';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
+import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
+
+import MUIButton from '@material-ui/core/Button';
+import { Col, Row, Form } from 'react-bootstrap';
+
+import { Typeahead } from 'react-bootstrap-typeahead';
+
+
+export const ImagePreview = ({ dataUri, handleRetakePhoto, handleAcceptPhoto, setGroup, setFullname, fullname, groups, selectedGroup }) => {
+
+  return (
+    <div>
+      <Row style={{color: 'white', paddingBottom: 15}} className="justify-content-row">
+        <Col md={6}>
+          Preview:
+        </Col>
+      </Row>
+      <Row className="justify-content-center">
+        <Col md={6}>
+          <div className="IconRetake">
+            <MUIButton onClick={handleRetakePhoto}>
+              <HighlightOffIcon style={{ fontSize: "4rem" }} />
+            </MUIButton>
+          </div>
+          <div className="IconAccept">
+            <MUIButton onClick={handleAcceptPhoto} >
+              <CheckCircleOutlineIcon style={{ fontSize: "4rem" }} />
+            </MUIButton>
+          </div>
+          <img src={dataUri} alt="waiver scanned" />
+        </Col>
+      </Row>
+      <Row className="row-fullname-ip">
+        <Col md={6}>
+          <Form.Group>
+            <Form.Label>Full Name:</Form.Label>
+            <Form.Control
+              name="fullname"
+              value={fullname}
+              onChange={(e) => {
+                setFullname(e.target.value)
+              }}
+              type="text"
+              autoComplete="off"
+              placeholder="Full Name"
+            />
+          </Form.Group>
+        </Col>
+      </Row>
+      <Row className="justify-content-row">
+        <Col md={6}>
+          <p className="p-groupname-rp">Rental Group Name: (If Applicable)</p>
+        </Col>
+      </Row>
+      <Row className="row-typeahead-rp justify-content-row">
+        <Col md={6}>
+          <Typeahead
+            labelKey="Groupname"
+            id="group-join"
+            onChange={(val) => {
+              setGroup(val)
+            }}
+            options={groups}
+            selected={selectedGroup}
+          />
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+ImagePreview.propTypes = {
+  dataUri: PropTypes.string,
+  isFullscreen: PropTypes.bool
+};
+
+export default ImagePreview;
