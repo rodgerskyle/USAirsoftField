d1b9e066f5f6639b3cdf8cc5271dc40ac325ad5f
Kyle Rodgers <kyle77r@gmail.com>
2021-02-26 13:03:24 -0800

    Updated styling of Contact us, added about us page

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -1,17 +1,6 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, InputGroup, FormControl, Button } from 'react-bootstrap/';
+import { Container, Row, Col } from 'react-bootstrap/';
 import logowbg from '../../assets/usairsoftwbg.png';
-import { Link } from "react-router-dom";
-import { AuthUserContext } from '../session';
-import { withFirebase } from '../Firebase';
-import * as ROLES from "../constants/roles";
-import fblogo from '../../assets/SocialMedia/facebook.png';
-import twlogo from '../../assets/SocialMedia/twitter.png';
-import iglogo from '../../assets/SocialMedia/instagram.png';
-import ytlogo from '../../assets/SocialMedia/youtube.png';
-import { encode } from 'firebase-encode';
-import logo from '../../assets/logo.png';
-
 
 class About extends Component {
     constructor(props) {
